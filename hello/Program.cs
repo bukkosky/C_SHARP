73908be365840545e3f092171e413f28849e1b3a
Marcin_ch <bukkosky@gmail.com>
2018-11-14 12:18:14 +0100

    Instrukcja If, pętla for

--- hello/Program.cs
+++ hello/Program.cs
@@ -6,7 +6,53 @@ namespace hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Witaj Świecie");
+            for(; ; )
+            {
+                Greetings();
+
+                Age();
+
+                Settings();
+
+            }
+        }
+
+        private static void Settings()
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Console.ResetColor();
+        }
+
+        private static void Age()
+        {
+            Console.Write("Wpisz ile masz lat ");
+            int age;
+
+            bool result = int.TryParse(Console.ReadLine(), out age);
+
+            if (age > 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Jesteś Pełnoletni ");
+            }
+            else if (result == false)
+            {
+                Console.WriteLine("Wprowadzono nieprawiłdowy wiek ");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Jesteś niepełnoletni ");
+            }
+        }
+
+        private static void Greetings()
+        {
+            Console.Write("Wpisz swoje imię ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Witaj " + name);
         }
     }
 }
