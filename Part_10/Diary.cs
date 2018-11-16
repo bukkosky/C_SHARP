9d6e8dfb4cf5b457a0062136995c721cc9a7ae1b
Marcin_ch <bukkosky@gmail.com>
2018-11-16 19:01:11 +0100

    variables and class difference

---
+++ Part_10/Diary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Part_10
+{
+    class Diary
+    {
+
+        public Diary()
+        {
+            ratings = new List<float>();
+        }
+
+        List<float> ratings;
+
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalculateAverage()
+        {
+
+            float sum = 0, avg = 0;
+
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+
+            avg = sum / ratings.Count;
+
+            return avg;
+        }
+
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
---
+++ Part_10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Part_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();
+            diary.AddRating(3);
+            diary.AddRating(5);
+
+            Diary diary2 = diary;
+            diary2.AddRating(8);
+        }
+    }
+}
